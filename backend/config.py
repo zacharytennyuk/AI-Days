966519867a5ae907117a93b37f87c0597f028046
John Miclat <miclatjohn1@gmail.com>
2024-10-26 17:35:52 -0400

    add comment

--- backend/config.py
+++ backend/config.py
@@ -2,12 +2,16 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    # Watson
     WATSON_API_KEY: str
     WATSON_SERVICE_URL: str
     WATSON_MODEL_ID: str
     WATSON_PROJECT_ID: str
+
+    # Slate
     SLATE_MODEL_ID: str
 
+    # Pinecone
     PINECONE_API_KEY: str
 
     class Config:
