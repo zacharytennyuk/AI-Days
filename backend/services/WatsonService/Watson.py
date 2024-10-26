418e7398bb634a074abb6251a45db153ff1db753
Max Martin <maxmartin54321@gmail.com>
2024-10-26 15:11:11 -0400

    removed ibm modules

--- backend/services/WatsonService/Watson.py
+++ backend/services/WatsonService/Watson.py
@@ -1,7 +1,5 @@
 import os
 from dotenv import load_dotenv
-from ibm_watson import LanguageTranslatorV3 
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import subprocess
 import json
 
