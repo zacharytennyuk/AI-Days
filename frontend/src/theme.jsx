607a3d8fa9676ea19b21548564a6592aebfc7107
Tuan Dang <tuan3dang@gmail.com>
2024-10-27 02:19:33 -0400

    Change colors

--- frontend/src/theme.jsx
+++ frontend/src/theme.jsx
@@ -1,33 +1,33 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 export const lightTheme = createTheme({
   palette: {
-    mode: 'light',
+    mode: "light",
     primary: {
-      main: '#7678ed',
+      main: "#005204",
     },
     secondary: {
-      main: '#dc004e',
+      main: "#dc004e",
     },
     background: {
-      default: '#87CEEB', // Sky blue for the entire page background
-      paper: '#FFFFFF', // White for components like the Navbar
+      default: "#f0ffd4", // Sky blue for the entire page background
+      paper: "#FFFFFF", // White for components like the Navbar
     },
   },
 });
 
 export const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      main: '#90caf9',
+      main: "#90caf9",
     },
     secondary: {
-      main: '#f48fb1',
+      main: "#f48fb1",
     },
     background: {
-      default: '#0D0D3B', // Darker midnight blue
-      paper: '#1A1A3F', // Slightly different shade for components
+      default: "#0D0D3B", // Darker midnight blue
+      paper: "#1A1A3F", // Slightly different shade for components
     },
   },
 });
