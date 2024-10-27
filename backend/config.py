7bae9a42060334ada82627a4a9c13f715781f926
John Miclat <miclatjohn1@gmail.com>
2024-10-26 20:14:37 -0400

    update keys

--- backend/config.py
+++ backend/config.py
@@ -5,14 +5,13 @@ class Settings(BaseSettings):
     # Watson
     WATSON_API_KEY: str
     WATSON_SERVICE_URL: str
-    WATSON_MODEL_ID: str
     WATSON_PROJECT_ID: str
-
-    # Slate
-    SLATE_MODEL_ID: str
 
     # Pinecone
     PINECONE_API_KEY: str
+
+    # Google
+    GOOGLE_PLACES_API_KEY: str
 
     class Config:
         env_file = ".env"
